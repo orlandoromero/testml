f58a6d3fe6bdfef5dfceb91fb352d5f2eb6e8ed8
Diana Catherine Cuellar Rivera <dianacuellar@MacBook-Air-de-Diana.local>
2021-01-13 01:34:45 -0500

    Anexo de routes para todas las vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { SearchResultsComponent } from './components/search-results/search-resul
 import { DetailProductComponent } from './components/detail-product/detail-product.component';
 
 const routes: Routes = [
-  { path: 'search-results', component: SearchResultsComponent},
-  { path: 'detail-product', component: DetailProductComponent}
+  { path: 'items?search=/:query', component: SearchResultsComponent},
+  { path: 'detail-product/:id', component: DetailProductComponent}
 ];
 
 @NgModule({
