51acb3905cded9f24d28da5ed5e77a3bb42b0965
Diana Catherine Cuellar Rivera <dianacuellar@MacBook-Air-de-Diana.local>
2021-01-14 13:59:13 -0500

    Actualización de paquetes y enrutadores del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { DetailProductComponent } from './components/detail-product/detail-product.component';
 
 const routes: Routes = [
+  { path: './', component: AppComponent},
   { path: 'items?search=/:query', component: SearchResultsComponent},
   { path: 'detail-product/:id', component: DetailProductComponent}
 ];
